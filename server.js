ec9c0fa04a07a1de5f1790940c5b3941a817d141
Ramso-dev <rossijatime@gmail.com>
2021-03-28 23:35:53 +0200

    fix server refresh

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var router = express.Router();
 
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-//view Engine. Serve the index from the angular main folder
-app.set('views', path.join(__dirname, 'src'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
 
 //Set static folder for angular stuff
 app.use(express.static(path.join(__dirname, 'dist/apa-tech-aufgabe')));
 
-// Body Parser Middelware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', index);
-//app.use('/api', routes);
+router.get('*', (req, res) => {
+  
+    res.sendFile(path.resolve(__dirname , 'dist/apa-tech-aufgabe/index.html'));
+});
 
+app.use('/', router);
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
